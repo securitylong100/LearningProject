834665d065b8d2c756375a76274883927597deaa
84097 <84097@DESKTOP-EUT0BSR>
2019-11-14 20:41:35 +0700

    update

---
+++ CrudInGridView/Default.aspx
@@ -0,0 +1,111 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CrudInGridView.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:GridView ID="gvPhoneBook" runat="server" AutoGenerateColumns="False" ShowFooter="True" DataKeyNames="StepID"
+                ShowHeaderWhenEmpty="True"
+
+                OnRowCommand="gvPhoneBook_RowCommand" OnRowEditing="gvPhoneBook_RowEditing" OnRowCancelingEdit="gvPhoneBook_RowCancelingEdit"
+                OnRowUpdating="gvPhoneBook_RowUpdating" OnRowDeleting="gvPhoneBook_RowDeleting"
+
+                BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="3">
+                <%-- Theme Properties --%>
+                <FooterStyle BackColor="White" ForeColor="#000066" />
+                <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="White" ForeColor="#000066" HorizontalAlign="Left" />
+                <RowStyle ForeColor="#000066" />
+                <SelectedRowStyle BackColor="#669999" Font-Bold="True" ForeColor="White" />
+                <SortedAscendingCellStyle BackColor="#F1F1F1" />
+                <SortedAscendingHeaderStyle BackColor="#007DBB" />
+                <SortedDescendingCellStyle BackColor="#CAC9C9" />
+                <SortedDescendingHeaderStyle BackColor="#00547E" />
+                
+                <Columns>
+                    <asp:TemplateField HeaderText="Step Name">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("StepName") %>' runat="server" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtStepName" Text='<%# Eval("StepName") %>' runat="server" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtStepNameFooter" runat="server" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Description">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("Description") %>' runat="server" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtDescription" Text='<%# Eval("Description") %>' runat="server" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtDescriptionFooter" runat="server" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Is Active">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("IsActive") %>' runat="server" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtIsActive" Text='<%# Eval("IsActive") %>' runat="server" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtIsActiveFooter" runat="server" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Last Update">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("LastUpdate") %>' runat="server" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtLastUpdate" Text='<%# Eval("LastUpdate") %>' runat="server" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtLastUpdateFooter" runat="server" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+                       <asp:TemplateField HeaderText="Last Update By">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("LastUpdateBy") %>' runat="server" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtLastUpdateBy" Text='<%# Eval("LastUpdateBy") %>' runat="server" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtLastUpdateByFooter" runat="server" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+                                        
+                    <asp:TemplateField>
+                        <ItemTemplate>
+                            <asp:ImageButton ImageUrl="~/Images/edit.png" runat="server" CommandName="Edit" ToolTip="Edit" Width="20px" Height="20px"/>
+                            <asp:ImageButton ImageUrl="~/Images/delete.png" runat="server" CommandName="Delete" ToolTip="Delete" Width="20px" Height="20px"/>
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:ImageButton ImageUrl="~/Images/save.png" runat="server" CommandName="Update" ToolTip="Update" Width="20px" Height="20px"/>
+                            <asp:ImageButton ImageUrl="~/Images/cancel.png" runat="server" CommandName="Cancel" ToolTip="Cancel" Width="20px" Height="20px"/>
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:ImageButton ImageUrl="~/Images/addnew.png" runat="server" CommandName="AddNew" ToolTip="Add New" Width="20px" Height="20px"/>
+                        </FooterTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+            <br />
+            <asp:Label ID="lblSuccessMessage" Text="" runat="server" ForeColor="Green" />
+            <br />
+            <asp:Label ID="lblErrorMessage" Text="" runat="server" ForeColor="Red" />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ CrudInGridView/Default.aspx.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CrudInGridView
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        string connectionString = @"Data Source=localhost;Initial Catalog=TEST;Integrated Security=True";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                PopulateGridview();
+            }
+        }
+
+        void PopulateGridview()
+        {
+            DataTable dtbl = new DataTable();
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("select * from ProcessStep", sqlCon);
+                sqlDa.Fill(dtbl);
+            }
+            if (dtbl.Rows.Count > 0)
+            {
+                gvPhoneBook.DataSource = dtbl;
+                gvPhoneBook.DataBind();
+            }
+            else
+            {
+                dtbl.Rows.Add(dtbl.NewRow());
+                gvPhoneBook.DataSource = dtbl;
+                gvPhoneBook.DataBind();
+                gvPhoneBook.Rows[0].Cells.Clear();
+                gvPhoneBook.Rows[0].Cells.Add(new TableCell());
+                gvPhoneBook.Rows[0].Cells[0].ColumnSpan = dtbl.Columns.Count;
+                gvPhoneBook.Rows[0].Cells[0].Text = "No Data Found ..!";
+                gvPhoneBook.Rows[0].Cells[0].HorizontalAlign = HorizontalAlign.Center;
+            }
+
+        }
+
+        protected void gvPhoneBook_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            try
+            {
+                if (e.CommandName.Equals("AddNew"))
+                {
+                    using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                    {
+                        sqlCon.Open();
+                        string query = "INSERT INTO ProcessStep (StepName, Description, IsActive, LastUpdate, LastUpdateBy) VALUES (@StepName,@Description,@IsActive,@LastUpdate, @LastUpdateBy)";
+                        SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                        sqlCmd.Parameters.AddWithValue("@StepName", (gvPhoneBook.FooterRow.FindControl("txtStepNameFooter") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@Description", (gvPhoneBook.FooterRow.FindControl("txtDescriptionFooter") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@IsActive", (gvPhoneBook.FooterRow.FindControl("txtIsActiveFooter") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@LastUpdate", (gvPhoneBook.FooterRow.FindControl("txtLastUpdateFooter") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@LastUpdateBy", (gvPhoneBook.FooterRow.FindControl("txtLastUpdateByFooter") as TextBox).Text.Trim());
+                        sqlCmd.ExecuteNonQuery();
+                        PopulateGridview();
+                        lblSuccessMessage.Text = "New Record Added";
+                        lblErrorMessage.Text = "";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                lblSuccessMessage.Text = "";
+                lblErrorMessage.Text = ex.Message;
+            }
+        }
+
+        protected void gvPhoneBook_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gvPhoneBook.EditIndex = e.NewEditIndex;
+            PopulateGridview();
+        }
+
+        protected void gvPhoneBook_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvPhoneBook.EditIndex = -1;
+            PopulateGridview();
+        }
+
+        protected void gvPhoneBook_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();
+                    string query = "UPDATE ProcessStep SET StepName=@StepName,Description=@Description,IsActive=@IsActive,LastUpdate=@LastUpdate ,LastUpdateBy=@LastUpdateBy WHERE StepID= @id";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                    sqlCmd.Parameters.AddWithValue("@StepName", (gvPhoneBook.Rows[e.RowIndex].FindControl("txtStepName") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Description", (gvPhoneBook.Rows[e.RowIndex].FindControl("txtDescription") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@IsActive", (gvPhoneBook.Rows[e.RowIndex].FindControl("txtIsActive") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@LastUpdate", (gvPhoneBook.Rows[e.RowIndex].FindControl("txtLastUpdate") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@LastUpdateBy", (gvPhoneBook.Rows[e.RowIndex].FindControl("txtLastUpdateBy") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@id", Convert.ToInt32(gvPhoneBook.DataKeys[e.RowIndex].Value.ToString()));
+                    sqlCmd.ExecuteNonQuery();
+                    gvPhoneBook.EditIndex = -1;
+                    PopulateGridview();
+                    lblSuccessMessage.Text = "Selected Record Updated";
+                    lblErrorMessage.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblSuccessMessage.Text = "";
+                lblErrorMessage.Text = ex.Message;
+            }
+        }
+        //shoul be import function checkdata format. but i don have time, i need look the footlball match bettwen VN and UAE, so i will update late.
+        bool checkdata()
+        {
+            return false;
+        }
+        protected void gvPhoneBook_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();
+                    string query = "DELETE FROM ProcessStep WHERE StepID = @id";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                    sqlCmd.Parameters.AddWithValue("@id", Convert.ToInt32(gvPhoneBook.DataKeys[e.RowIndex].Value.ToString()));
+                    sqlCmd.ExecuteNonQuery();
+                    PopulateGridview();
+                    lblSuccessMessage.Text = "Selected Record Deleted";
+                    lblErrorMessage.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblSuccessMessage.Text = "";
+                lblErrorMessage.Text = ex.Message;
+            }
+        }
+    }
+}
